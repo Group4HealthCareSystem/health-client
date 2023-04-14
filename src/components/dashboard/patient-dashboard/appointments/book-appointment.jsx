b033362a3226ff658274aee023555629b056f56a
Owolabi Celina <108185066+OwolabiCelina@users.noreply.github.com>
2023-04-14 11:10:44 +0100

    dashboard-added

--- src/components/dashboard/patient-dashboard/appointments/book-appointment.jsx
+++ src/components/dashboard/patient-dashboard/appointments/book-appointment.jsx
@@ -1,7 +1,8 @@
-// import "./BookAppointment.css"
 import React, { useState, useEffect } from 'react';
+import "./book-appointment.css";
+import Header from '../../../../reusables/header/header';
 
-function BookAppointment() {
+const BookAppointment = () => {
   const [patients, setPatients] = useState([]);
   const [doctors, setDoctors] = useState([]);
   const [selectedPatient, setSelectedPatient] = useState('');
@@ -12,98 +13,104 @@ function BookAppointment() {
   const [status, setStatus] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:8000/appointment/appointment/')
-      .then(response => response.json())
-      .then(data => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/appointment/appointment/');
+        const data = await response.json();
         setPatients(data.map(appointment => appointment.patient));
         setDoctors(data.map(appointment => appointment.doctor));
-      })
-      .catch(error => console.error(error));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
   }, []);
 
-  const handleSubmit = event => {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
-    // Send POST request to create appointment
-    const newAppointment = {
-      patient: selectedPatient,
-      doctor: selectedDoctor,
-      date_time: dateTime,
-      duration,
-      message,
-      status,
-    };
-    fetch('http://localhost:8000/appointment/appointment/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newAppointment),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Appointment created:', data);
-        // Reset form fields
-        setSelectedPatient('');
-        setSelectedDoctor('');
-        setDateTime('');
-        setDuration('');
-        setMessage('');
-        setStatus('');
-      })
-      .catch(error => console.error(error));
+    try {
+      const newAppointment = {
+        patient: selectedPatient,
+        doctor: selectedDoctor,
+        date_time: dateTime,
+        duration,
+        message,
+        status,
+      };
+      const response = await fetch('http://localhost:8000/appointment/appointment/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newAppointment),
+      });
+      const data = await response.json();
+      console.log('Appointment created:', data);
+      setSelectedPatient('');
+      setSelectedDoctor('');
+      setDateTime('');
+      setDuration('');
+      setMessage('');
+      setStatus('');
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <label>
-        Patient:
-        <select value={selectedPatient} onChange={event => setSelectedPatient(event.target.value)}>
-          {patients.map(patient => (
-            <option key={patient.id} value={patient.id}>
-              {patient.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <br />
-      <label>
-        Doctor:
-        <select value={selectedDoctor} onChange={event => setSelectedDoctor(event.target.value)}>
-          {doctors.map(doctor => (
-            <option key={doctor.id} value={doctor.id}>
-              {doctor.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <br />
-      <label>
-        Date and time:
-        <input type="datetime-local" value={dateTime} onChange={event => setDateTime(event.target.value)} />
-      </label>
-      <br />
-      <label>
-        Duration (in minutes):
-        <input type="number" value={duration} onChange={event => setDuration(event.target.value)} />
-      </label>
-      <br />
-      <label>
-        Message:
-        <textarea value={message} onChange={event => setMessage(event.target.value)} />
-      </label>
-      <br />
-      <label>
-        Status:
-        <select value={status} onChange={event => setStatus(event.target.value)}>
-          <option value="pending">Pending</option>
-          <option value="confirmed">Confirmed</option>
-          <option value="cancelled">Cancelled</option>
-        </select>
-      </label>
-      <br />
-      <button type="submit">Book Appointment</button>
-    </form>
+      <div className='book-appointment'>
+        <Header />
+        <form className="form" onSubmit={handleFormSubmit}>
+          <label>
+            Patient:
+            <select value={selectedPatient} onChange={(event) => setSelectedPatient(event.target.value)}>
+              {patients.map(patient => (
+                <option key={patient.id} value={patient.id}>
+                  {patient.name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <br />
+          <label>
+            Doctor:
+            <select value={selectedDoctor} onChange={(event) => setSelectedDoctor(event.target.value)}>
+              {doctors.map(doctor => (
+                <option key={doctor.id} value={doctor.id}>
+                  {doctor.name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <br />
+          <label>
+            Date and time:
+            <input type="datetime-local" value={dateTime} onChange={(event) => setDateTime(event.target.value)} />
+          </label>
+          <br />
+          <label>
+            Duration (in minutes):
+            <input type="number" value={duration} onChange={(event) => setDuration(event.target.value)} />
+          </label>
+          <br />
+          <label>
+            Message:
+            <textarea value={message} onChange={(event) => setMessage(event.target.value)} />
+          </label>
+          <br />
+          <label>
+            Status:
+            <select value={status} onChange={(event) => setStatus(event.target.value)}>
+              <option value="pending">Pending</option>
+              <option value="confirmed">Confirmed</option>
+              <option value="cancelled">Cancelled</option>
+            </select>
+          </label>
+          <br />
+          <button type="submit">Book Appointment</button>
+        </form>
+        </div>
   );
-}
+};
 
 export default BookAppointment;
